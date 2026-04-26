c5616cc0f6189d9357abae9cc45cb1376343f32e
Poorav999 <poorav.sharma@live.com>
2026-04-26 21:40:09 +0530

    fix: razorpay + env issue

--- app/api/razorpay/route.ts
+++ app/api/razorpay/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import Razorpay from "razorpay";
 
+export const dynamic = "force-dynamic";
+
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID!,
   key_secret: process.env.RAZORPAY_KEY_SECRET!,
